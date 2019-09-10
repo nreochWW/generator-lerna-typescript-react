c3869f4274ae80dcd96d42ad9e5e14f3a966a57e
Nigel.Reoch <nigel.reoch@ww.com>
2019-09-10 14:14:20 -0400

    finished main app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -47,6 +47,42 @@ module.exports = class extends Generator {
       this.destinationPath("yarn.lock")
     );
     this.fs.copy(this.templatePath(".npmrc"), this.destinationPath(".npmrc"));
+    this.fs.copy(
+      this.templatePath(".gitignore"),
+      this.destinationPath(".gitignore")
+    );
+    this.fs.copy(
+      this.templatePath(".prettierrc"),
+      this.destinationPath(".prettierrc")
+    );
+    this.fs.copy(
+      this.templatePath(".stylelintrc"),
+      this.destinationPath(".stylelintrc")
+    );
+    this.fs.copy(
+      this.templatePath(".storybook/*"),
+      this.destinationPath(".storybook")
+    );
+    this.fs.copy(
+      this.templatePath("packages/*"),
+      this.destinationPath("packages")
+    );
+    this.fs.copy(
+      this.templatePath("jest.config.js"),
+      this.destinationPath("jest.config.js")
+    );
+    this.fs.copy(
+      this.templatePath("lerna.json"),
+      this.destinationPath("lerna.json")
+    );
+    this.fs.copy(
+      this.templatePath("tsconfig.json"),
+      this.destinationPath("tsconfig.json")
+    );
+    this.fs.copy(
+      this.templatePath("tslint.json"),
+      this.destinationPath("tslint.json")
+    );
   }
   conflicts() {}
   install() {
